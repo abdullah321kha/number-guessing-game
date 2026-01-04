54ef94330e62d0803514657e9267228c6a1a6301
Abdullah Imran <154907949+abdullah321kha@users.noreply.github.com>
2026-01-04 13:05:55 +0500

    Add files via upload

---
+++ rand.py
@@ -0,0 +1,14 @@
+import random
+
+secret_number = random.randint(1, 10)
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+
+    if guess == secret_number:
+        print("🎉 Correct! You guessed the number.")
+        break
+    elif guess > secret_number:
+        print("Too high. Try again.")
+    else:
+        print("Too low. Try again.")
